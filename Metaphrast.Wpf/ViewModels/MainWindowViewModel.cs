91af8191a2562116cea9655e306e4733f690aa07
Andreas Sekulski <andreas.sekulski@gmail.com>
2022-01-08 23:16:46 +0100

    First gui approach

--- Metaphrast.Wpf/ViewModels/MainWindowViewModel.cs
+++ Metaphrast.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,36 +1,31 @@
-﻿using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using Prism.Mvvm;
 
 namespace Metaphrast.Wpf.ViewModels
 {
-    public class Model
-    {
-        public string Name { get; set; }
-        public List<string> Values { get; set; } = new();
-    }
-
     public class MainWindowViewModel : BindableBase
     {
+        private string _title = "Metaphrast Translation App";
+
         public DataTable Data { get; set; } = new();
 
-        private string _title = "Prism Application";
         public string Title
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get => _title;
+            set => SetProperty(ref _title, value);
         }
 
         public MainWindowViewModel()
         {
 
             Data.Columns.Add("Key");
-            Data.Columns.Add("*English*");
+            Data.Columns.Add("English");
             Data.Columns.Add("German");
             Data.Columns.Add("Italia");
             Data.Columns.Add("France");
 
-            Data.Rows.Add(new object[] { "England", "England", "England", "Inghilterra", "Angleterre" });
+            for(int i = 0; i < 100; i++ )
+                Data.Rows.Add("England", "England", "England", "Inghilterra", "Angleterre");
         }
     }
 }
